599da7a6df3a9b6dc6d72a4126c80fa5a72f68c4
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-04-01 16:08:33 +0530

    api call in backend using axios

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 const ChatPage = () => {
-  const fetchChats = () => {};
+  const fetchChats = async () => {
+    const data = await axios.get("/api/chat");
 
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
   return <div> ChatPage</div>;
 };
 
