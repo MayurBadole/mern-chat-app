018473b4aa4fcc785683f59517d3687a73e7ef08
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-04-02 21:15:05 +0530

    connected db and colors

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { chats } = require("./data/data");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("api is started");
@@ -19,4 +22,4 @@ app.get("/api/chat/:id", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, console.log(`server started on PORT ${PORT}`));
+app.listen(5000, console.log(`server started on PORT ${PORT}`.blue.bold));
